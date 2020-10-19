c77d2517136648b2edee535a2884f841ab00fe4e
Daniel Vega <danielalexvega@gmail.com>
2020-10-19 14:33:34 -0500

    add yargs commands

--- app.js
+++ app.js
@@ -1,10 +1,56 @@
 const validator = require('validator');
 const notes = require('./notes');
 const chalk = require('chalk');
+const yargs = require('yargs');
 
-// console.log(notes());
-// console.log(validator.isEmail('danielalexvega@gmail.com'));
-// console.log(validator.isURL('http://www.danielalexvega.com'));
+// add, remove, read, list
 
-console.log(chalk.bold.bgBlue.underline.white('Success!'));
-console.log(chalk.bold.bgRed.underline.black('Success!'));
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: (argv) => {
+    console.log('Title: ' + argv.title);
+    console.log('Body: ' + argv.body);
+  }
+});
+
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: () => {
+    console.log('Removing a note!');
+  }
+});
+
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: () => {
+    console.log('Reading a note!');
+  }
+});
+
+yargs.command({
+  command: 'list',
+  describe: 'List out all the notes',
+  handler: () => {
+    console.log('Listing out all the notes');
+  }
+});
+
+yargs.parse();
+
+// console.log(yargs.argv);
