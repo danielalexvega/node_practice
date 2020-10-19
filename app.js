9ff87636d438e9591da6b5091f9480ff73eb9f31
Daniel Vega <danielalexvega@gmail.com>
2020-10-19 12:07:16 -0500

    more practice

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const {name, add} = require('./utils');
+const validator = require('validator');
 const notes = require('./notes');
+const chalk = require('chalk');
 
-console.log(name);
-console.log(add(3, 4));
-console.log(notes());
+// console.log(notes());
+// console.log(validator.isEmail('danielalexvega@gmail.com'));
+// console.log(validator.isURL('http://www.danielalexvega.com'));
+
+console.log(chalk.bold.bgBlue.underline.white('Success!'));
+console.log(chalk.bold.bgRed.underline.black('Success!'));
--- utils.js
+++
@@ -1,6 +0,0 @@
-console.log('utils.js');
-const name = 'Dylan';
-
-const add = (a, b) => a + b;
-
-module.exports = {name, add};
