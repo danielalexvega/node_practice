ba72756eab6758184c0669d2c5bad7ff405c3f13
Daniel Vega <danielalexvega@gmail.com>
2020-10-19 10:57:05 -0500

    udemy practice

---
+++ app.js
@@ -0,0 +1,6 @@
+const {name, add} = require('./utils');
+const notes = require('./notes');
+
+console.log(name);
+console.log(add(3, 4));
+console.log(notes());
---
+++ utils.js
@@ -0,0 +1,6 @@
+console.log('utils.js');
+const name = 'Dylan';
+
+const add = (a, b) => a + b;
+
+module.exports = {name, add};
